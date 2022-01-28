0a613fd05855880ea1a2ea36f1a01194aeabd1ee
David Hecker <heckerdw@gmail.com>
2022-01-27 19:32:46 -0800

    cleanup.

--- pythonisms/pythonisms.py
+++ pythonisms/pythonisms.py
@@ -1,14 +1,10 @@
-def say_hi():
-    return 'hi'
-
-
 def count_down(func):
     def inner(*args, **kwargs):
         print(3)
         print(2)
         print(1)
         return func(*args, **kwargs)
-        
+
     return inner
 
 @count_down
